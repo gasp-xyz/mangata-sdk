93ee09b87de23f9e4c039f34e78f63ad5ef1e5a8
Michal <devdanco@gmail.com>
2021-07-08 11:54:56 +0200

    Refactor tx and nonce

--- src/services/Query.ts
+++ src/services/Query.ts
@@ -1,7 +1,6 @@
 import { ApiPromise } from '@polkadot/api'
 import { AccountInfo } from '@polkadot/types/interfaces'
 import BN from 'bn.js'
-import memoryDatabase from '../utils/MemoryDatabase'
 
 type Iquery = {
   getNonce(api: ApiPromise, address: string): Promise<BN>
@@ -9,23 +8,7 @@ type Iquery = {
 
 const getNonce = async (api: ApiPromise, address: string): Promise<BN> => {
   const accountInfo: AccountInfo = await api.query.system.account(address)
-  const onchainNonce: BN = accountInfo.nonce.toBn()
-
-  let nonce: BN
-
-  if (!memoryDatabase.hasAddressNonce(address)) {
-    nonce = onchainNonce
-  } else {
-    nonce = memoryDatabase.getNonce(address)
-  }
-
-  if (onchainNonce.gt(nonce)) {
-    nonce = onchainNonce
-  }
-
-  const nextNonce: BN = nonce.addn(1)
-  memoryDatabase.setNonce(address, nextNonce)
-  return nonce
+  return accountInfo.nonce.toBn()
 }
 
 export const Query: Iquery = {
--- src/services/Tx.ts
+++ src/services/Tx.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 import { ApiPromise } from '@polkadot/api'
 import { SubmittableExtrinsic } from '@polkadot/api/types'
 import BN from 'bn.js'
+import memoryDatabase from '../utils/MemoryDatabase'
 import { Query } from './Query'
 
 type Itx = {
@@ -14,8 +16,32 @@ type Itx = {
   ): Promise<void>
 }
 
-const signTx = async (tx: SubmittableExtrinsic<'promise'>, address: string, nonce?: BN) => {
-  await tx.signAndSend(address, { nonce })
+const signTx = async (tx: SubmittableExtrinsic<'promise'>, address: string, onChainNonce?: BN) => {
+  let nonce: BN
+  if (onChainNonce && !memoryDatabase.hasAddressNonce(address)) {
+    nonce = onChainNonce
+  } else {
+    nonce = memoryDatabase.getNonce(address)
+  }
+
+  if (onChainNonce && onChainNonce.gt(nonce)) {
+    nonce = onChainNonce
+  }
+
+  const nextNonce: BN = nonce.addn(1)
+  memoryDatabase.setNonce(address, nextNonce)
+
+  await tx.signAndSend(address, { nonce }, ({ status, isError }) => {
+    console.log('Transaction status:', status.type)
+    if (status.isInBlock) {
+      console.log('Included at block hash', status.asInBlock.toHex())
+    } else if (status.isFinalized) {
+      console.log('Finalized block hash', status.asFinalized.toHex())
+    } else if (isError) {
+      console.log('Transaction error')
+      memoryDatabase.setNonce(address, nextNonce)
+    }
+  })
 }
 
 const createPool = async (
