e9255cb43cd4f992c0e0b8f36630a510e78bc22b
Michal <devdanco@gmail.com>
2021-12-09 17:09:59 +0100

    fix eslint

--- test/instance.test.ts
+++ test/instance.test.ts
@@ -2,8 +2,8 @@ import { Mangata } from '../src'
 
 describe('Test for for instances', () => {
   it('should get different instances for different uri', async () => {
-    let developMangata = Mangata.getInstance('develop')
-    let productionMangata = Mangata.getInstance('production')
+    const developMangata = Mangata.getInstance('develop')
+    const productionMangata = Mangata.getInstance('production')
     expect(developMangata.getUri()).not.toBe(productionMangata.getUri())
   })
 })
