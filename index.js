0f812b6f4b57e62dfd5878b8d32a96cf8851660b
Michal <devdanco@gmail.com>
2022-05-13 10:17:51 +0200

    new build index.js

--- index.js
+++ index.js
@@ -171,8 +171,12 @@ const balancesMap = async (api) => {
 const accountEntriesMap = async (api, address) => {
     const ownedAssetsResponse = await api.query.tokens.accounts.entries(address);
     return ownedAssetsResponse.reduce((acc, [key, value]) => {
-        const id = key.toHuman()[1].replace(/[, ]/g, '');
-        const balance = new BN(BigInt(JSON.parse(JSON.stringify(value)).free).toString());
+        const free = JSON.parse(JSON.stringify(value)).free.toString();
+        const frozen = JSON.parse(JSON.stringify(value)).frozen.toString();
+        const freeBN = isHex(free) ? hexToBn(free) : new BN(free);
+        const frozenBN = isHex(frozen) ? hexToBn(frozen) : new BN(frozen);
+        const id = key.toHuman()[1].replace(/[, ]/g, "");
+        const balance = freeBN.sub(frozenBN);
         acc[id] = balance;
         return acc;
     }, {});
