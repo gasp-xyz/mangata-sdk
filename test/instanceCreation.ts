486a693beb32fca7c2110e884b9deae5a1aca965
Michal <devdanco@gmail.com>
2022-06-12 10:06:09 +0200

    es and cjs format

--- test/instanceCreation.ts
+++ test/instanceCreation.ts
@@ -1,6 +1,6 @@
 import { Mangata } from "../index";
 
-const uri = process.env.API_URL ? process.env.API_URL : "ws://127.0.0.1:9955";
+const uri = process.env.API_URL ? process.env.API_URL : "ws://127.0.0.1:8844";
 
 export const instance = Mangata.getInstance(uri);
 export const SUDO_USER_NAME = process.env.TEST_SUDO_NAME
