a185d78e915c23bd87b6af4cbe9bd6b82f1d0ddd
devdanco <devdanco@gmail.com>
2022-11-30 07:52:44 +0100

    chore: fix small test

--- test/node-version.test.ts
+++ test/node-version.test.ts
@@ -1,8 +1,8 @@
 import { it, expect, afterAll } from "vitest";
 import { instance } from "./instanceCreation";
 
-it("should match version 0.4.0 node version when calling getNodeVersion method", async () => {
-  const version = "0.1.0-unknown-x86_64-linux-gnu";
+it("should match node version when calling getNodeVersion method", async () => {
+  const version = "0.1.0";
   const nodeVersion = await instance.getNodeVersion();
   expect(nodeVersion).toMatch(new RegExp(`^${version}?`));
 });
