ec0db5819b3db52571f7d37e554525abdeac78b1
devdanco <devdanco@gmail.com>
2023-03-09 13:03:34 +0100

    update gitignore

--- node_modules/@polkadot/x-randomvalues/react-native.d.ts
+++
@@ -1,3 +0,0 @@
-import { getRandomValues as getRandomValuesGlobal } from './browser';
-export { packageInfo } from './packageInfo';
-export declare const getRandomValues: typeof getRandomValuesGlobal;
