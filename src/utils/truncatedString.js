8e9ec1bd4e5245a46809e3bf7b6477b670352703
devdanco <devdanco@gmail.com>
2023-03-09 12:46:37 +0100

    fix issue with emit files

--- src/utils/truncatedString.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.truncatedString = void 0;
-var truncatedString = function (str) {
-    if (!str)
-        return "";
-    return (str.substring(0, 7) + "..." + str.substring(str.length - 5, str.length));
-};
-exports.truncatedString = truncatedString;
