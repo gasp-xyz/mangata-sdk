8eb9e69270df18d9c56b6e58c3d19b6f2e8ac1fa
devdanco <devdanco@gmail.com>
2023-04-04 15:36:28 +0200

    fix: ksm deposit

--- src/services/Tx.ts
+++ src/services/Tx.ts
@@ -261,56 +261,47 @@ export class Tx {
     const kusamaApi = await new ApiPromise({ provider, noInitWarn: true })
       .isReady;
 
-    const destination = {
-      V1: {
-        interior: {
-          X1: {
-            ParaChain: parachainId
-          }
-        },
-        parents: 0
-      }
-    };
-
-    const beneficiary = {
-      V1: {
-        interior: {
-          X1: {
-            AccountId32: {
-              id: kusamaApi
-                .createType("AccountId32", destinationMangataAddress)
-                .toHex(),
-              network: "Any"
-            }
-          }
-        },
-        parents: 0
-      }
-    };
-
-    const assets = {
-      V1: [
-        {
-          fun: {
-            Fungible: amount
-          },
-          id: {
-            Concrete: {
-              interior: "Here",
-              parents: 0
-            }
-          }
-        }
-      ]
-    };
-
     await kusamaApi.tx.xcmPallet
-      .reserveTransferAssets(destination, beneficiary, assets, 0)
+      .limitedReserveTransferAssets(
+        {
+          V3: {
+            parents: 0,
+            interior: {
+              X1: { Parachain: parachainId }
+            }
+          }
+        },
+        {
+          V3: {
+            parents: 0,
+            interior: {
+              X1: {
+                AccountId32: {
+                  id: kusamaApi
+                    .createType("AccountId32", destinationMangataAddress)
+                    .toHex()
+                }
+              }
+            }
+          }
+        },
+        {
+          V3: [
+            {
+              id: { Concrete: { parents: 0, interior: "Here" } },
+              fun: { Fungible: amount }
+            }
+          ]
+        },
+        0,
+        "Unlimited"
+      )
       .signAndSend(ksmAccount, {
         signer: txOptions?.signer,
         nonce: txOptions?.nonce
       });
   }
+
   static async sendKusamaTokenFromParachainToRelay(
     api: ApiPromise,
     mangataAccount: string | KeyringPair,
