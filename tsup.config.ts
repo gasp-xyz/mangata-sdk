5d1488611548f5642b0043ed9956bb1e7df5cd16
devdanco <devdanco@gmail.com>
2023-05-24 13:56:23 +0200

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,8 @@ import type { Options } from "tsup";
 
 const config: Options = {
   entry: ["src/index.ts"],
+  splitting: false,
+  // minify: true,
   dts: true,
   format: ["esm"],
   clean: true
