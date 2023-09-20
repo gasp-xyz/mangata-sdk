9f8deb12e42aaffeff772d0191a4a1bd8134dbb2
devdanco <devdanco@gmail.com>
2023-09-20 08:22:32 +0000

    Deploying to gh-pages from @ mangata-finance/mangata-sdk@86e9960e01d9ae1031ed29919895b0da60ab3171 🚀

--- src/utils/isInputValid.ts
+++
@@ -1,5 +0,0 @@
-export const isInputValid = (value: string): boolean => {
-  const valueNum = +value;
-
-  return !(!value || isNaN(Number(value)) || isNaN(valueNum) || valueNum < 0);
-};
--- src/utils/truncatedString.ts
+++
@@ -1,6 +0,0 @@
-export const truncatedString = (str: string) => {
-  if (!str) return "";
-  return (
-    str.substring(0, 7) + "..." + str.substring(str.length - 5, str.length)
-  );
-};
